1da82955ffacfe62c2df3cf5514d8dccf1e76fe5
francescofoti <francesco.foti@devinfo.net>
2022-05-14 09:42:41 +0200

    Switching to PellesC compiler

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-SimpleConsole
+# SimpleConsole
+
+This is a C application using the consoul.net dll to display and interact with a *Consoul* console window.
+
+Originally created with Visual Studio, it has been converted for the PellesC development environment (also use of <tchar.h> dropped).
+SimpleConsole.exe has no other dependencies than the *Consoul* dll [which can be downloaded from here](https://consoul.net/docs/downloads/).
+
+More information on the [consoul.net C docs](https://consoul.net/docs/c/) page.
--- SimpleConsole/callbacks.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-UINT STDCALL OnConsoulMouseButton(HWND hWndConsole, UINT wEvtCode, UINT wParam, UINT wZoneID, UINT wLine, UINT wCol, UINT wPosX, UINT wPosY);
-UINT STDCALL OnConsoulVirtualLine(HWND hWnd, ULONG dwLine); // warning, know issue, should be UINT wLine
-UINT STDCALL OnConsoulZonePaint(HWND hWnd, HDC hDC, UINT wZoneID, ULONG dwLine, UINT wLeft, UINT wTop, UINT wRight, UINT wBottom);
-UINT STDCALL OnConsoulWmPaint(HWND hWndConsole, UINT wCbkMode, HDC hDC, const LPRECT lprcLinePos, const LPRECT lprcLineRect, const LPRECT lprcPaint);
-
-void OnMouseButtonMessage(HWND hWndConsole, PTCSMOUSEEVENT pEvtData, WPARAM wParam);
-void OnZoneDrawMessage(HWND hWndConsole, PTCSODZONEDRAW pEvtData);
